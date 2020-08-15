5c90de9bcb16cef296ce8f3ed3aa5a6a41f8fee0
chick <chick_0@ch0.kr>
2020-08-15 11:25:36 +0900

    minor fix

--- data/command/info.py
+++ data/command/info.py
@@ -14,13 +14,13 @@ async def main(message, client):
     app = await client.application_info()
     filters = json.load(open("data/cache__filters.json", mode="r", encoding="utf-8"))
 
-    await message.channel.send("```"
+    await message.channel.send("``\n`"
                                f"Connected to {len(client.guilds)} guilds\n"
                                f"BOT Owner: {app.owner}\n\n"
                                f"Filter words: {len(filters)}\n  -> use '{prefix}filter' command to check it!"
                                "```")
     if message.author.id == app.owner.id:
-        await message.channel.send("```"
+        await message.channel.send("```\n"
                                    f"Cached Cat: {len(cat_cache.get_cache_list())}\n"
                                    f"Cache Limit: {option.cache_limit}\n"
                                    f"Cache Size: {round(cat_cache.get_cache_size() / (1024 * 1024), 2)} MB\n"
