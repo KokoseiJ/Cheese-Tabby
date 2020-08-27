62cd93925940146457a67f4790a4134df6768e28
chick <chick_0@ch0.kr>
2020-08-27 14:24:31 +0900

    use_cache_only mode added

--- data/lib/core.py
+++ data/lib/core.py
@@ -2,8 +2,8 @@
 
 import logging
 
-import option
 from data.lib import api, img_cache
+import option
 
 logger = logging.getLogger()
 
@@ -16,29 +16,40 @@ async def get():
         logger.info("Set timeout to default value [3] ")
         tm_out = 3
 
-    cat_worker = await api.get_image(
-        api_url=option.api_url,
-        tm_out=tm_out
-    )
+    if option.use_cache_only is False:
+        cat_worker = await api.get_image(
+            api_url=option.api_url,
+            tm_out=tm_out
+        )
 
-    if cat_worker[0] is False:
-        logging.info("Try to use Cat Cache...")
+        if cat_worker[0] is False:
+            logging.info("Try to use image cache...")
+            if len(img_cache.get_cache_list()) == 0:
+                logging.warning("Cache Directory is EMPTY!")
+                logging.warning("Send Error Message!")
+                return f"**WARNING! API SERVER ERROR!**\n - {cat_worker[1]}"
+            else:
+                return await img_cache.get_cat_random()
+        elif cat_worker[0] is True:
+            if option.cache_limit > len(img_cache.get_cache_list()):
+                logger.info("Adding Cat to 'cat_cache'...")
+                await img_cache.save_cat(cat_worker[1])
+            else:
+                logger.info("Cache is full!")
+                if option.replace_on_limit is True:
+                    logger.info("Replace option is enabled!")
+                    logger.info("Adding Cat to 'cat_cache'...")
+                    await img_cache.replace_cat(cat_worker[1])
+                pass
+
+            return cat_worker[1]
+    else:
         if len(img_cache.get_cache_list()) == 0:
-            logging.warning("'cat_cache' is EMPTY!")
+            logging.warning("Cache Directory is EMPTY!")
             logging.warning("Send Error Message!")
-            return f"**WARNING! API SERVER ERROR!**\n - {cat_worker[1]}"
+
+            return "**Cache is EMPTY!!!**\n" \
+                   "add image at cache directory or turn off the use_cache_only mode"
         else:
             return await img_cache.get_cat_random()
-    elif cat_worker[0] is True:
-        if option.cache_limit > len(img_cache.get_cache_list()):
-            logger.info("Adding Cat to 'cat_cache'...")
-            await img_cache.save_cat(cat_worker[1])
-        else:
-            logger.info("Cache is full!")
-            if option.replace_on_limit is True:
-                logger.info("Replace option is enabled!")
-                logger.info("Adding Cat to 'cat_cache'...")
-                await img_cache.replace_cat(cat_worker[1])
-            pass
 
-        return cat_worker[1]
