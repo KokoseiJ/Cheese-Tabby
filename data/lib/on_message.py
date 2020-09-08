2316b3e0f5238da87efc998583bda715e9cd87d7
chick_0 <chick_0@kakao.com>
2020-09-08 21:50:14 +0900

    bug fixed

--- data/lib/on_message.py
+++ data/lib/on_message.py
@@ -19,7 +19,7 @@ async def private(message: discord.message):
         )
 
     msg_content = message.content
-    for block_item in json.load(open_it(filename="data/filter/cache__block_words.json")):
+    for block_item in json.load(open_it(filename="data/cache__block_words.json")):
         msg_content = msg_content.replace(block_item, "")
 
     for item in json.load(open_it(filename="data/cache__filters.json")):
