13fe3a6414c7f307d1ed85569952bcac0dd57eb8
chick_0 <chick_0@kakao.com>
2020-09-08 21:42:56 +0900

    minor update

--- data/command/owner.py
+++ data/command/owner.py
@@ -13,53 +13,41 @@ class Owner(commands.Cog, name="for Bot OWNER"):
     @commands.command(help="Shutdown the bot")
     @commands.is_owner()
     async def close(self, ctx: commands.context):
-        if await ctx.bot.is_owner(user=ctx.author):
-            await ctx.send(":wave:")
-            await ctx.bot.close()
-        else:
-            logger.warning(f"[{ctx.author.id}]{ctx.author} try to use owner command")
-            await ctx.send("?")
+        await ctx.send(":wave:")
+        await ctx.bot.close()
 
     @commands.command(help="Delete duplicate images from the cache folder")
     @commands.is_owner()
     async def clear(self, ctx: commands.context):
-        if await ctx.bot.is_owner(user=ctx.author):
-            before = len(img_cache.get_cache_list())
-            await img_cache.purge_same()
-            after = len(img_cache.get_cache_list())
+        before = len(img_cache.get_cache_list())
+        await img_cache.purge_same()
+        after = len(img_cache.get_cache_list())
 
-            await ctx.send(
-                "```\n"
-                f" - {before - after} Deleted!"
-                "```"
-            )
-        else:
-            logger.warning(f"[{ctx.author.id}]{ctx.author} try to use owner command")
-            await ctx.send("?")
+        await ctx.send(
+            "```\n"
+            f" - {before - after} Deleted!"
+            "```"
+        )
 
     @commands.command(help="Delete all images in the cache folder")
     @commands.is_owner()
     async def clear_cache(self, ctx: commands.context):
-        if await ctx.bot.is_owner(user=ctx.author):
-            logger.info("Removing cached image from cache directory...")
+        logger.info("Removing cached image from cache directory...")
 
-            cc = len(img_cache.get_cache_list())
-            logger.info(f"'{cc}' detected")
+        cc = len(img_cache.get_cache_list())
+        logger.info(f"'{cc}' detected")
 
-            if cc == 0:
-                logger.info("cache directory is already empty")
-                await ctx.send(
-                    "```\n"
-                    " - Already empty!"
-                    "```"
-                )
-            else:
-                img_cache.purge_cache()
-                await ctx.send(
-                    "```\n"
-                    f" - {cc} Deleted!"
-                    "```"
-                )
+        if cc == 0:
+            logger.info("cache directory is already empty")
+            await ctx.send(
+                "```\n"
+                " - Already empty!"
+                "```"
+            )
         else:
-            logger.warning(f"[{ctx.author.id}]{ctx.author} try to use owner command")
-            await ctx.send("?")
+            img_cache.purge_cache()
+            await ctx.send(
+                "```\n"
+                f" - {cc} Deleted!"
+                "```"
+            )
